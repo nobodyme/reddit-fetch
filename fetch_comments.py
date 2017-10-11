19a7b7db22ff3c6313072c14f75dbd2234ad06ec
nobodyme <nobodydisturbme@gmail.com>
2017-10-11 04:28:39 +0000

    Initial commit

---
+++ fetch_comments.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import argparse
+import sys
+import json
+import requests
+		
+def get_response():
+		
+  url = sys.argv[1]
+  response = requests.get(url+'.json',headers = {'User-agent':'bot'})
+		
+  if not response.ok:
+    print ("Error ", response.status_code)
+    exit(response.status_code)
+  
+  post_fetch = list()      
+  data = response.json()
+  title = data[0]['data']['children'][0]['data']['title']
+  post_fetch.append(title+'\n\n')
+  for i in range(0,50):
+    post_fetch.append(data[1]['data']['children'][i]['data']['body']+'\n')
+  return post_fetch
+
+def main():
+
+  output_filehandle = open("posts.txt",
+                           mode = 'w',
+                           encoding = 'utf8')
+  top_level_comments = get_response()
+  output_text = '\n'.join(top_level_comments)
+  output_filehandle.write(output_text)
+  output_filehandle.write('\n')
+
+if __name__ == '__main__':
+  main()
