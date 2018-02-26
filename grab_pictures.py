598e0c4f00848c16a135f19062751882bba46bbb
nobodyme <naveenkumarspa@gmail.com>
2018-02-26 19:56:37 +0530

    Made modular

--- grab_pictures.py
+++ grab_pictures.py
@@ -19,33 +19,7 @@ def erase_previous_line():
     sys.stdout.write("\033[K")
 
 
-ua = UserAgent()
-parser = argparse.ArgumentParser(description='Fetch images from a subreddit')
-parser.add_argument('-s', '--subreddit', nargs='+', type=str, metavar='',
-                    required=True, help='Exact name of the subreddits you want to grab pictures')
-parser.add_argument('-n', '--number', type=int, metavar='', default=50,
-                    help='Optionally specify number of images to be downloaded (default=50)')
-parser.add_argument('-t', '--top', type=str, metavar='', choices=['day', 'week', 'month', 'year', 'all'],
-                    default='week', help='optionally specify whether top posts of [day, week, month, year or all] (default=week)')
-args = parser.parse_args()
-
-for j in range(len(args.subreddit)):
-    print('Connecting to r/' + args.subreddit[j])
-    url = 'https://www.reddit.com/r/' + args.subreddit[j] + '/top/.json?sort=top&t=' + \
-        args.top + '&limit=' + str(args.number)
-    response = requests.get(url, headers={'User-agent': ua.random})
-
-    if not response.ok:
-        print("Error check the name of the subreddit", response.status_code)
-        exit(response.status_code)
-
-    if not os.path.exists(args.subreddit[j]):
-        os.mkdir(args.subreddit[j])
-    # notify connected and downloading pictures from subreddit
-    erase_previous_line()
-    print('downloading pictures from r/' + args.subreddit[j] + '..')
-
-    data = response.json()['data']['children']
+def get_pictures_from_subreddit(data, subreddit):
     for i in range(len(data)):
         current_post = data[i]['data']
         image_url = current_post['url']
@@ -63,7 +37,42 @@ for j in range(len(args.subreddit)):
         image = requests.get(image_url, allow_redirects=False)
         if(image.status_code == 200):
             output_filehandle = open(
-                args.subreddit[j] + '/' + get_valid_filename(current_post['title']) + extension, mode='wb')
+                subreddit + '/' + get_valid_filename(current_post['title']) + extension, mode='wb')
             output_filehandle.write(image.content)
-    erase_previous_line()
-    print('Downloaded pictures from r/' + args.subreddit[j])
+
+
+def main():
+    ua = UserAgent()
+    parser = argparse.ArgumentParser(description='Fetch images from a subreddit')
+    parser.add_argument('-s', '--subreddit', nargs='+', type=str, metavar='',
+                        required=True, help='Exact name of the subreddits you want to grab pictures')
+    parser.add_argument('-n', '--number', type=int, metavar='', default=50,
+                        help='Optionally specify number of images to be downloaded (default=50)')
+    parser.add_argument('-t', '--top', type=str, metavar='', choices=['day', 'week', 'month', 'year', 'all'],
+                        default='week', help='optionally specify whether top posts of [day, week, month, year or all] (default=week)')
+    args = parser.parse_args()
+
+    for j in range(len(args.subreddit)):
+        print('Connecting to r/' + args.subreddit[j])
+        url = 'https://www.reddit.com/r/' + args.subreddit[j] + '/top/.json?sort=top&t=' + \
+            args.top + '&limit=' + str(args.number)
+        response = requests.get(url, headers={'User-agent': ua.random})
+
+        if not response.ok:
+            print("Error check the name of the subreddit", response.status_code)
+            exit(response.status_code)
+
+        if not os.path.exists(args.subreddit[j]):
+            os.mkdir(args.subreddit[j])
+        # notify connected and downloading pictures from subreddit
+        erase_previous_line()
+        print('downloading pictures from r/' + args.subreddit[j] + '..')
+
+        data = response.json()['data']['children']
+        get_pictures_from_subreddit(data,args.subreddit[j])
+        erase_previous_line()
+        print('Downloaded pictures from r/' + args.subreddit[j])
+
+
+if __name__ == '__main__':
+    main()
